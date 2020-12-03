0f71d9245425c8fb4797d52ba91b888f4f411f4d
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-12-02 21:44:17 -0600

    feat: go over react basics on rendering JSX via import and export, importing react-bootstrap and axios, learning how to use props to have an array of objects and use .map() to extract properties of an object

---
+++ pokemon/src/components/App.js
@@ -0,0 +1,45 @@
+//1. import
+import React from 'react';
+import NavbarIndex from "./NavbarIndex";
+import Info from "./Info";
+
+
+//2. create function to return JSX
+const App = props => {
+    const pokeMon = [
+        {name:"Ivysaur", description:"green pokemon", type:"grass"},
+        {name:"Charmeleon", description:"lizard pokemon", type:"fire"},
+        {name:"Wartortle", description:"turtle pokemon", type:"water"}];
+    // const renderNames = pokeMon.map(({name,description,type}) => {
+    //     return (
+    //         <div>
+    //             <h1>{name}</h1>
+    //             <p>{description}</p>
+    //             <h6>{type}</h6>
+    //         </div>
+    //     )
+    const renderNames = pokeMon.map(pokemon => {
+        return (
+            <div key={pokemon.name}>
+                <h1>{pokemon.name}</h1>
+                <p>{pokemon.description}</p>
+                <h6>{pokemon.type}</h6>
+            </div>
+        )
+    })
+    return (
+        <div>
+            <NavbarIndex/>
+            <h1>Yo Yo Yo</h1>
+            <br/>
+            <p>Love from Mars!</p>
+            <Info name={"Bulbasaur"} description={"green pokemon"} type={"grass"}/>
+            <Info name={"Charmander"} description={"lizard pokemon"} type={"fire"}/>
+            <Info name={"Squirtle"} description={"turtle pokemon"} type={"water"}/>
+            <div>{renderNames}</div>
+        </div>
+    )
+}
+
+//3. export the function
+export default App;
