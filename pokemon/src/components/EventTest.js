3641dd985d247eea9100118d7350947598a03ccc
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-12-05 12:46:40 -0600

    feat: create three examples of state changes, changing a rectangle's color according to the index value, and a dynamic input that will display the value of the input in the rectangle

--- pokemon/src/components/EventTest.js
+++ pokemon/src/components/EventTest.js
@@ -2,19 +2,46 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 
 class EventTest extends React.Component {
-state = {color : 'blue'}
+    state = {color: 'blue', index: 0, input: "Test 1"}
+
     renderRectangle() {
+        const listColors = ['blue', 'red', 'green', 'orange'];
         return (
-            <div style={{backgroundColor: this.state.color, padding : "100px"}}>
-                <p>This is a test</p>
+            <div className={"my-2"} style={{backgroundColor: listColors[this.state.index], padding: "100px"}}>
+                <p>{this.state.input}</p>
             </div>
         )
     }
 
+    changeColor() {
+        if (this.state.index === 3)
+            this.setState({index: 0});
+        else {
+            let number = this.state.index;
+            number++;
+            this.setState({index: number});
+            console.log(this.state.index);
+        }
+    }
+
+    //create an input that will change the text inside of the rectangle live - utilize onChange()
+    inputObject() {
+        return (
+            <div className={"my-2"}>
+                <label className={"mr-2"}>Dynamic Input:</label>
+                <input onChange={(event) => this.dynamicInput(event.target.value)} type="text"/>
+            </div>
+        )
+    }
+
+    dynamicInput(value) {
+        this.setState({input : value});
+    }
+
     renderRedButton() {
         return (
-            <div>
-                <button type={'button'}>Change to Red</button>
+            <div className={"my-2"}>
+                <button onClick={() => this.changeColor()} type={'button'}>Change to Red</button>
             </div>
         )
     }
@@ -22,10 +49,12 @@ state = {color : 'blue'}
     render() {
         return (
             <Container>
+                {this.inputObject()}
                 {this.renderRectangle()}
                 {this.renderRedButton()}
             </Container>
-    )}
+        )
+    }
 }
 
 export default EventTest;
