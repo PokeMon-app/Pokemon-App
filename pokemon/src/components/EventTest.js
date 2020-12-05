676db205cfd3406edf5d3fb7e5df34e5175e74e0
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-12-05 11:48:51 -0600

    feat: refactor render to call renderRectangle() so the contents will be rendered

--- pokemon/src/components/EventTest.js
+++ pokemon/src/components/EventTest.js
@@ -3,12 +3,18 @@ import Container from 'react-bootstrap/Container';
 
 class EventTest extends React.Component {
 
+    renderRectangle() {
+        return (
+            <div style={{backgroundColor: "blue", padding : "100px"}}>
+                <p>This is a test</p>
+            </div>
+        )
+    }
+
     render() {
         return (
             <Container>
-                <div style={{backgroundColor: "blue", padding : "100px"}}>
-                    <p>This is a test</p>
-                </div>
+                {this.renderRectangle()}
             </Container>
     )}
 }
