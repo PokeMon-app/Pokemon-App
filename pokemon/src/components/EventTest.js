7e02a95af3c90be68cdf308eb463f2591e0ac0c6
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-12-05 11:43:16 -0600

    feat: convert EventTest to now implement a class that will utilize a render method

--- pokemon/src/components/EventTest.js
+++ pokemon/src/components/EventTest.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 
-const EventTest = () => {
-    return (
-        <Container>
-            <div style={{backgroundColor: "blue", padding : "100px"}}>
-                <p>This is a test</p>
-            </div>
-        </Container>
-    )
+class EventTest extends React.Component {
+
+    render() {
+        return (
+            <Container>
+                <div style={{backgroundColor: "blue", padding : "100px"}}>
+                    <p>This is a test</p>
+                </div>
+            </Container>
+    )}
 }
 
 export default EventTest;
