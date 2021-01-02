f65eee863e73b391606d5f658cb12ab0d9be0e81
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-01-02 17:15:49 -0600

    feat: have functionality that will transform the search icon to an input and line up the dropdown items

--- pokemon/src/components/NavbarIndex.js
+++ pokemon/src/components/NavbarIndex.js
@@ -1,7 +1,10 @@
 import React, {useState, useRef, useEffect} from 'react';
 import pokeball from './Images/pokeball1.png';
+import allTypes from './Images/all_types.png';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faSearch} from '@fortawesome/free-solid-svg-icons'
+import {faCaretRight} from '@fortawesome/free-solid-svg-icons'
+import {faUserCircle} from '@fortawesome/free-solid-svg-icons'
 import {Link} from 'react-router-dom';
 import Modal from "react-bootstrap/Modal";
 import TypeModal from "./TypeModal";
@@ -9,12 +12,26 @@ import TypeModal from "./TypeModal";
 const NavbarIndex = () => {
     const [showModal, setShowModal] = useState(false);
     const [isOpen, setIsOpen] = useState(false);
+    const [searchStatus, setSearchStatus] = useState(false);
 
     const callback = (isOpen, showModal) => {
         setIsOpen(!isOpen);
         setShowModal(!showModal);
     }
 
+
+
+    const searchTransition = (searchStatus) => {
+        setSearchStatus(!searchStatus)
+        console.log(searchStatus)
+        if(isOpen == true) {
+            setSearchStatus(!searchStatus);
+
+        }
+        else if(isOpen == false) {
+            setSearchStatus(!searchStatus)
+        }
+    }
     let modalNode = useRef(null);
     let pokeballNode = useRef(null);
     //without any effects:
@@ -77,20 +94,22 @@ const NavbarIndex = () => {
         if (isOpen === true) {
             return (
                 <div ref={pokeballNode} className={'pokeball-menu'}>
-                    <ul>
-                        <li className={"dropdown-link"}>
-                            <Link to={"/profile"} onClick={() => setIsOpen(!isOpen)} className={"nav-links"}
-                            >Profile</Link>
-                        </li>
-                        <li className={"dropdown-link"}>
-                            <button className={"nav-links"}>Search</button>
-                        </li>
-                        <li className={"dropdown-link"}>
-                            <button onClick={() => setShowModal(!showModal)
-                            }>Filter Type
-                            </button>
-                        </li>
-                    </ul>
+                        <div className={"dropdown-link align-self-center"}>
+                            <Link to={"/profile"} onClick={() => setIsOpen(!isOpen)}
+                            ><FontAwesomeIcon icon={faUserCircle} size="2x" className={"profile-link"}></FontAwesomeIcon></Link>
+                        </div>
+                        <div className={"dropdown-link d-flex" + (searchStatus ? "" : " center-icon")}>
+                            <FontAwesomeIcon icon={faSearch} size="2x" onClick={() => {searchTransition(searchStatus);}}
+                            className={"nav-links circular align-self-center hover" + (searchStatus ? " search-hide" : " search-show")}></FontAwesomeIcon>
+                            <div className={"search-container d-flex align-self-center" + (!searchStatus ? " search-hide" : " search-show")}>
+                                <FontAwesomeIcon icon={faCaretRight} size="2x" className={"align-self-center hover " + (!searchStatus ? "search-hide" : "search-show")} onClick={() => {setSearchStatus(false)}}></FontAwesomeIcon>
+                                <input className={"nav-links round-input" + (!searchStatus ? " search-hide" : " search-show")} placeholder={"Search..."} type={"text"}/>
+                            </div>
+                        </div>
+                        <div className={"dropdown-link align-self-center"}>
+                                <img className={(!showModal ? "all-types" : "all-types-active") + " p-1"} onClick={() => setShowModal(!showModal)
+                                } src={allTypes} alt="Filter by types"/>
+                        </div>
                 </div>
             )
         }
@@ -102,6 +121,9 @@ const NavbarIndex = () => {
             <button className={"buttonInner" + (!isOpen ? " closedPokeball" : " openPokeball")}
                     type={'button'} onClick={() => {
                 setIsOpen(!isOpen)
+                if(isOpen != true) {
+                    setSearchStatus(false)
+                }
             }}>
                 <img className={"pokeball"} src={pokeball} alt="pokeball"/>
             </button>
