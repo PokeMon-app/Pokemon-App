90612bdfb49a1359293d1b935fa99a622da2ab18
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-01-05 21:25:30 -0600

    feat: create method that stores a pokemon object to Gen1Pokemon array, but it only stores one pokemon at a time and doesn't store each pokemon...

--- pokemon/src/components/GetPokemon.js
+++ pokemon/src/components/GetPokemon.js
@@ -1,138 +1,63 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
-const GetPokemon = () => {
+function GetPokemon() {
     //can refactor this method to take in params based on the position of the scroll to make another get for the next gen of pokemon...
     // may need two seperate methods to persist the previous gen list
-    const [setPokemon, setGen1] = useState([]);
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [Gen1Pokemon, setGen1Pokemon] = useState([]);
+    // const [items, setItems] = useState([]);
 
+    let Gen2Url = "https://pokeapi.co/api/v2/pokemon?offset=151&limit=100";
+    let Gen3Url = "https://pokeapi.co/api/v2/pokemon?offset=251&limit=135";
+    let Gen4Url = "https://pokeapi.co/api/v2/pokemon?offset=386&limit=108";
+    let Gen5Url = "https://pokeapi.co/api/v2/pokemon?offset=495&limit=154";
+    let Gen6Url = "https://pokeapi.co/api/v2/pokemon?offset=649&limit=72";
+    let Gen7Url = "https://pokeapi.co/api/v2/pokemon?offset=721&limit=88";
+    let Gen8Url = "https://pokeapi.co/api/v2/pokemon?offset=809&limit=89";
 
-    const GetGen1 = async () => {
-        const response = await
-            axios.get("https://pokeapi.co/api/v2/pokemon?limit=151")
-        return response.data.results;
-    };
-    GetGen1().then(pokeList => {
-        let names = [];
-        for (let pokemon of pokeList) {
-            names.push(pokemon.name)
-        }
-        constructButtons(names).then(res => {
-            console.log(res)
-        });
-    });
+    useEffect(() => {
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+            .then(res => res.json())
+            .then(
+                (data) => {
+                    setIsLoaded(true);
+                    // console.log(data.results)
+                    // setItems(data.results);
+                    for(let pokemon of data.results) {
+                        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+                            .then(results => results.json())
+                            .then(pokemon => {
+                                let newPokemon = {id: pokemon.id, name: pokemon.name, image: pokemon.sprites.front_default};
+                                console.log(Gen1Pokemon)
+                                setGen1Pokemon([...Gen1Pokemon, newPokemon]);
+                                console.log(Gen1Pokemon)
+                            })
+                    }
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
 
-    const GetGen2 = async () => {
-        const response = await
-            axios.get("https://pokeapi.co/api/v2/pokemon?offset=151&limit=100")
-        return response.data.results;
-    };
-    GetGen2().then(pokeList => {
-        let names = [];
-        for (let pokemon of pokeList) {
-            names.push(pokemon.name)
-        }
-        return names;
-    });
-
-    const GetGen3 = async () => {
-        const response = await
-            axios.get("https://pokeapi.co/api/v2/pokemon?offset=251&limit=135")
-        return response.data.results;
-    };
-    GetGen3().then(pokeList => {
-        let names = [];
-        for (let pokemon of pokeList) {
-            names.push(pokemon.name)
-        }
-        return names;
-    });
-
-    const GetGen4 = async () => {
-        const response = await
-            axios.get("https://pokeapi.co/api/v2/pokemon?offset=386&limit=108")
-        return response.data.results;
-    };
-    GetGen4().then(pokeList => {
-        let names = [];
-        for (let pokemon of pokeList) {
-            names.push(pokemon.name)
-        }
-        return names;
-    });
-
-    const GetGen5 = async () => {
-        const response = await
-            axios.get("https://pokeapi.co/api/v2/pokemon?offset=495&limit=154")
-        return response.data.results;
-    };
-    GetGen5().then(pokeList => {
-        let names = [];
-        for (let pokemon of pokeList) {
-            names.push(pokemon.name)
-        }
-        return names;
-    });
-
-    const GetGen6 = async () => {
-        const response = await
-            axios.get("https://pokeapi.co/api/v2/pokemon?offset=649&limit=72")
-        return response.data.results;
-    };
-    GetGen6().then(pokeList => {
-        let names = [];
-        for (let pokemon of pokeList) {
-            names.push(pokemon.name)
-        }
-        return names;
-    });
-
-    const GetGen7 = async () => {
-        const response = await
-            axios.get("https://pokeapi.co/api/v2/pokemon?offset=721&limit=88")
-        return response.data.results;
-    };
-    GetGen7().then(pokeList => {
-        let names = [];
-        for (let pokemon of pokeList) {
-            names.push(pokemon.name)
-        }
-        return names;
-    });
-
-    const GetGen8 = async () => {
-        const response = await
-            axios.get("https://pokeapi.co/api/v2/pokemon?offset=809&limit=89")
-        return response.data.results;
-    };
-    GetGen8().then(pokeList => {
-        let names = [];
-        for (let pokemon of pokeList) {
-            names.push(pokemon.name)
-        }
-        return names;
-    });
-
-    const constructButtons = async nameList => {
-        let buildList = [];
-        for(let placeName of nameList) {
-            // console.log(name);
-            const response = await
-                axios.get(`https://pokeapi.co/api/v2/pokemon/${placeName}`)
-            let id = response.data.id;
-            let name = response.data.name;
-            let img = response.data.sprites.front_default;
-            let pokeArray = [id, name, img];
-            buildList.push(pokeArray);
-        }
-        return buildList;
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <ul>
+                {Gen1Pokemon.map(pokemon => {
+                    return  <li key={pokemon.name}>
+                                {pokemon.name}
+                            </li>
+                })}
+            </ul>
+        )
     }
-
-    return (
-        <div>
-            {setPokemon}
-        </div>
-    )
 }
 
 export default GetPokemon;
